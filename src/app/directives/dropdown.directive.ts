7fa2185b3cb97ca535c7d23fdcc33b768e7d0909
Iurii Prytula <iprytula@insert-ad.insert.intranet>
2019-12-12 14:18:07 +0100

    modify my custom directive

--- src/app/directives/dropdown.directive.ts
+++ src/app/directives/dropdown.directive.ts
@@ -1,29 +1,14 @@
-import { Directive, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-
   constructor(private renderer: Renderer2, private elRef: ElementRef) { }
 
-  @HostListener('click', ['$event']) click(e) {
-    if (e.target.classList.contains('show')) {
-      this.renderer.removeClass(this.elRef.nativeElement, 'show');
-      this.renderer.removeClass(
-        this.renderer.nextSibling(this.elRef.nativeElement),
-        'show'
-      );
-    } else {
-      this.renderer.addClass(this.elRef.nativeElement, 'show');
-      this.renderer.addClass(
-        this.renderer.nextSibling(this.elRef.nativeElement),
-        'show'
-      );
-    }
-  }
+  @HostBinding('class.show') isOpen = false;
 
-  // @HostListener('document:click', ['$event']) clickout(e) {
-  //   console.log(e.target.classList);
-  // }
+  @HostListener('click') toogleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }
