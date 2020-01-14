94cbe312cf23df7c9327c4042bb49b7ac52c0376
Iurii Prytula <iprytula@insert-ad.insert.intranet>
2020-01-14 09:38:15 +0100

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './components/routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
